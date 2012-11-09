daf83e3235f33c5cee09c330a19b6092cbdfe273
Gerke Geurts <ggeurts@geurtsrus.com>
2012-11-09 14:35:16 +0100

    Added inline code documentation.

--- src/WebApiContrib.Formatting.Html/Formatters/HtmlMediaTypeViewFormatter.cs
+++ src/WebApiContrib.Formatting.Html/Formatters/HtmlMediaTypeViewFormatter.cs
@@ -7,7 +7,6 @@ using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using System.Linq;
 using WebApiContrib.Formatting.Html.Common;
 using WebApiContrib.Formatting.Html.Configuration;
 using WebApiContrib.Formatting.Html.Locators;
@@ -15,12 +14,21 @@ using WebApiContrib.Formatting.Html.ViewParsers;
 
 namespace WebApiContrib.Formatting.Html.Formatters
 {
+    /// <summary>
+    /// A <see cref="MediaTypeFormatter"/> that produces HTML content.
+    /// </summary>
     public class HtmlMediaTypeViewFormatter : MediaTypeFormatter
     {
         private readonly string _siteRootPath;
         private readonly IViewLocator _viewLocator;
         private readonly IViewParser _viewParser;
 
+        /// <summary>
+        /// Creates a new <see cref="HtmlMediaTypeViewFormatter"/> instance.
+        /// </summary>
+        /// <param name="siteRootPath">Physical or virtual root path for resolution of view templates.</param>
+        /// <param name="viewLocator">The view locator to use for retrieval of view templates.</param>
+        /// <param name="viewParser">The view parser to use for generation of views from view templates.</param>
         public HtmlMediaTypeViewFormatter(string siteRootPath = null, IViewLocator viewLocator = null, IViewParser viewParser = null)
         {
             SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
@@ -35,22 +43,55 @@ namespace WebApiContrib.Formatting.Html.Formatters
             _siteRootPath = siteRootPath;
         }
 
-  
+        /// <summary>
+        /// Queries whether this <see cref="T:System.Net.Http.Formatting.MediaTypeFormatter"/> can serializean object of the specified type.
+        /// </summary>
+        /// <param name="type">The type to serialize.</param>
+        /// <returns>
+        /// true if the <see cref="T:System.Net.Http.Formatting.MediaTypeFormatter"/> can serialize the type; otherwise, false.
+        /// </returns>
         public override bool CanWriteType(Type type)
         {
             return true;
         }
 
+        /// <summary>
+        /// Queries whether this <see cref="T:System.Net.Http.Formatting.MediaTypeFormatter"/> can deserializean object of the specified type.
+        /// </summary>
+        /// <param name="type">The type to deserialize.</param>
+        /// <returns>
+        /// true if the <see cref="T:System.Net.Http.Formatting.MediaTypeFormatter"/> can deserialize the type; otherwise, false.
+        /// </returns>
         public override bool CanReadType(Type type)
         {
             return false;
         }
 
+        /// <summary>
+        /// Asynchronously deserializes an object of the specified type.
+        /// </summary>
+        /// <param name="type">The type of the object to deserialize.</param>
+        /// <param name="readStream">The <see cref="T:System.IO.Stream"/> to read.</param>
+        /// <param name="content">The <see cref="T:System.Net.Http.HttpContent"/>, if available. It may be null.</param>
+        /// <param name="formatterLogger">The <see cref="T:System.Net.Http.Formatting.IFormatterLogger"/> to log events to.</param>
+        /// <returns>
+        /// A <see cref="T:System.Threading.Tasks.Task"/> whose result will be an object of the given type.
+        /// </returns>
+        /// <exception cref="T:System.NotSupportedException">Derived types need to support reading.</exception>
         public override Task<object> ReadFromStreamAsync(Type type, Stream readStream, HttpContent content, IFormatterLogger formatterLogger)
         {
             throw new NotSupportedException();
         }
 
+        /// <summary>
+        /// Returns a specialized instance of the <see cref="T:System.Net.Http.Formatting.MediaTypeFormatter"/> that can format a response for the given parameters.
+        /// </summary>
+        /// <param name="type">The type to format.</param>
+        /// <param name="request">The request.</param>
+        /// <param name="mediaType">The media type.</param>
+        /// <returns>
+        /// Returns <see cref="T:System.Net.Http.Formatting.MediaTypeFormatter"/>.
+        /// </returns>
         public override MediaTypeFormatter GetPerRequestFormatterInstance(Type type, HttpRequestMessage request, MediaTypeHeaderValue mediaType)
         {
             if (_viewLocator == null || _viewParser == null)
@@ -77,6 +118,18 @@ namespace WebApiContrib.Formatting.Html.Formatters
             return base.GetPerRequestFormatterInstance(type, request, mediaType);
         }
 
+        /// <summary>
+        /// Asynchronously writes an object of the specified type.
+        /// </summary>
+        /// <param name="type">The type of the object to write.</param>
+        /// <param name="value">The object value to write.  It may be null.</param>
+        /// <param name="writeStream">The <see cref="T:System.IO.Stream"/> to which to write.</param>
+        /// <param name="content">The <see cref="T:System.Net.Http.HttpContent"/> if available. It may be null.</param>
+        /// <param name="transportContext">The <see cref="T:System.Net.TransportContext"/> if available. It may be null.</param>
+        /// <returns>
+        /// A <see cref="T:System.Threading.Tasks.Task"/> that will perform the write.
+        /// </returns>
+        /// <exception cref="T:System.NotSupportedException">Derived types need to support writing.</exception>
         public override Task WriteToStreamAsync(Type type, object value, Stream writeStream, HttpContent content, TransportContext transportContext)
         {
             return TaskHelpers.RunSync(() =>
@@ -92,7 +145,8 @@ namespace WebApiContrib.Formatting.Html.Formatters
 
         private byte[] ParseView(Type type, object model, Encoding encoding)
         {
-            var view = model as IView ?? new View(GetViewName(model), model, type);
+            var viewName = GetViewName(model != null ? model.GetType() : type);
+            var view = model as IView ?? new View(viewName, model, type);
 
             var viewTemplate = ViewLocator.GetView(_siteRootPath, view);
 
@@ -127,16 +181,11 @@ namespace WebApiContrib.Formatting.Html.Formatters
             }
         }
 
-        private static string GetViewName(object model)
+        private static string GetViewName(Type modelType)
         {
-            var modelType = model.GetType();
-
-            var viewAttributes = modelType.GetCustomAttributes(typeof (ViewAttribute), true)
-                                          .OfType<ViewAttribute>()
-                                          .ToArray();
-
-            if (viewAttributes.Any())
-                return viewAttributes.First().ViewName;
+            var viewAttributes = (ViewAttribute[])modelType.GetCustomAttributes(typeof(ViewAttribute), true);
+            if (viewAttributes.Length > 0)
+                return viewAttributes[0].ViewName;
             
             if (GlobalViews.Views.ContainsKey(modelType))
                 return GlobalViews.Views[modelType];
--- src/WebApiContrib.Formatting.Html/ViewParsers/IViewParser.cs
+++ src/WebApiContrib.Formatting.Html/ViewParsers/IViewParser.cs
@@ -2,8 +2,20 @@
 
 namespace WebApiContrib.Formatting.Html.ViewParsers
 {
+    /// <summary>
+    /// Interface for view template parsers. A view template 
+    /// parser transforms abstract views into concrete views
+    /// based using view templates.
+    /// </summary>
     public interface IViewParser
     {
+        /// <summary>
+        /// Generates concrete view for a given abstract view.
+        /// </summary>
+        /// <param name="view">The abstract view.</param>
+        /// <param name="viewTemplate">The view template to be used to generate the concrete view.</param>
+        /// <param name="encoding">The text encoding of the concrete view.</param>
+        /// <returns>Byte representation of the generated concrete view.</returns>
         byte[] ParseView(IView view, string viewTemplate, Encoding encoding);
     }
 }
